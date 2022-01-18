b21722d97ed6b5d82820da4b46801b57f8f672c0
IvanJoksimovic <37101344+IvanJoksimovic@users.noreply.github.com>
2022-01-18 09:40:53 +0100

    Added progress bar functionalty to data read

--- SPOD.py
+++ SPOD.py
@@ -9,6 +9,7 @@ import sys
 import shutil
 import argparse
 import math
+from tqdm import tqdm
 
 DATA_INPUT_METHOD = "foo"
 
@@ -17,43 +18,43 @@ class DATA_INPUT_FUNCTIONS:
     # Most simple method, file contains only one column
     def readSingleColumn(path):
         data = np.genfromtxt(path,delimiter=None)
-        print(path)
+        #print(path)
         return data
     # Usually openFoam raw output method 
     def readOpenFOAMRawFormatVector_ComponentZ(path):
         data = np.genfromtxt(path,delimiter=None,skip_header=2)
-        print(path)
+        #print(path)
         return data[:,-1]
     # Usually openFoam raw output method 
     def readOpenFOAMRawFormatVector_ComponentY(path):
         data = np.genfromtxt(path,delimiter=None,skip_header=2)
-        print(path)
+        #print(path)
         return data[:,-2]
     # Usually openFoam raw output method 
     def readOpenFOAMRawFormatVector_ComponentX(path):
         data = np.genfromtxt(path,delimiter=None,skip_header=2)
-        print(path)
+        #print(path)
         return data[:,-3]
     # Usually openFoam raw output method 
     def readFirstColumn(path):
         data = np.genfromtxt(path,delimiter=None,skip_header=2)
-        print(path)
+        #print(path)
         return data[:,0]
     # Usually openFoam raw output method 
     def readSecondColumn(path):
         data = np.genfromtxt(path,delimiter=None,skip_header=2)
-        print(path)
+        #print(path)
         return data[:,1]
     # Usually openFoam raw output method 
     def readThirdColumn(path):
         data = np.genfromtxt(path,delimiter=None,skip_header=2)
-        print(path)
+        #print(path)
         return data[:,2]
     # Usually openFoam raw output method 
     def readAllThreeVectorComponents(path):
         data = np.genfromtxt(path,delimiter=None,skip_header=2)
         data = data[:,-3:]
-        print(path)
+        #print(path)
         return data.flatten('F')
     
 def dataInput(path):
@@ -203,9 +204,14 @@ def main():
     start = time.perf_counter()
     
     R = []
+    '''
     with Executor() as executor:
         for r in executor.map(dataInput,timePaths):
             R.append(r)
+    '''
+    print("Reading data files...")
+    with Executor() as executor:
+        R = list(tqdm(executor.map(dataInput, timePaths), total=len(timePaths)))
                 
     finish = time.perf_counter()
     print("===========================================================")
